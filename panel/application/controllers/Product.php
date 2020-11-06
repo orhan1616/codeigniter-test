547001fed0d9f6be408917c87c2a3fdec822749f
Orhan Soyer <codewebturkey@gmail.com>
2020-11-07 01:58:37 +0300

    ilk-proje

---
+++ panel/application/controllers/Product.php
@@ -0,0 +1,80 @@
+<?php
+
+class Product extends CI_Controller
+{
+    public $viewFolder = "";
+
+    public function __construct()
+    {
+
+        parent::__construct();
+
+        $this->viewFolder = "product_v";
+
+        $this->load->model("product_model");
+
+    }
+
+    public function index(){
+
+        $viewData = new stdClass();
+
+        /** Tablodan Verilerin Getirilmesi.. */
+        $items = $this->product_model->get_all();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "list";
+        $viewData->items = $items;
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+    }
+
+
+    public function new_form(){
+
+        $viewData = new stdClass();
+
+        /** View'e gönderilecek Değişkenlerin Set Edilmesi.. */
+        $viewData->viewFolder = $this->viewFolder;
+        $viewData->subViewFolder = "add";
+
+        $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+
+    }
+
+    public function save(){
+
+        $this->load->library("form_validation");
+
+        // Kurallar yazilir..
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required"  => "<b>{field}</b> alanı doldurulmalıdır"
+            )
+        );
+
+        // Form Validation Calistirilir..
+        // TRUE - FALSE
+        $validate = $this->form_validation->run();
+
+        if($validate){
+
+            echo "Kayit İşlemleri başlar...";
+            
+        } else {
+
+            echo validation_errors();
+        }
+
+        // Başarılı ise
+            // Kayit işlemi baslar
+        // Başarısız ise
+            // Hata ekranda gösterilir...
+
+    }
+
+
+}
